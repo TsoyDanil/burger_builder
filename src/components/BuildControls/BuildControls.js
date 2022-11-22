5dbb014a0285c866f327e0362f3bb92296adee18
Mishakov Pavel <pashamishakov@gmail.com>
2022-11-22 15:21:10 +0600

    finished classwork 55

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl'
 
 const BuildControls = props => {
-  return null;
+  return (
+    <div className='BuildControls'>
+      <p>Current Price: <strong>{props.price} TG</strong></p>
+      {Object.keys(props.ingredients).map(ingType => {
+        return <BuildControl 
+          key={ingType} 
+          type={ingType} 
+          added={() => props.ingredientAdded(ingType)}
+          removed={() => props.ingredientRemoved(ingType)}
+          disabledInfo={props.disabledInfo[ingType]}
+        />
+      })}
+    </div>
+  )
 }
 
 export default BuildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,68 @@
 import React from 'react';
+import { useState } from 'react';
+import BuildControls from '../../components/BuildControls/BuildControls';
+import Burger from '../../components/Burger/Burger'
 
 const BurgerBuilder = () => {
+  const [ingredients, setIngredients] = useState({
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+  })
+  const [totalPrice, setTotalPrice] = useState(200)
+
+  const INGREDIENT_PRICES = {
+    salad: 50,
+    bacon: 300,
+    cheese: 200,
+    meat: 500
+  }
+
+  const addIngredientHandler = (type) => {
+    const oldCount = ingredients[type]
+    const updateCount = oldCount + 1
+    const updatedIngredients = {...ingredients}
+    updatedIngredients[type] = updateCount
+
+    const priceAddition = INGREDIENT_PRICES[type]
+    const newPrice = totalPrice + priceAddition
+
+    setIngredients(updatedIngredients)
+    setTotalPrice(newPrice)
+  }
+
+  const removeIngredientHandler = (type) => {
+    const oldCount = ingredients[type]
+    if (oldCount <= 0) return
+
+    const updateCount = oldCount - 1
+    const updatedIngredients = {...ingredients}
+    updatedIngredients[type] = updateCount
+
+    const priceAddition = INGREDIENT_PRICES[type]
+    const newPrice = totalPrice - priceAddition
+
+    setIngredients(updatedIngredients)
+    setTotalPrice(newPrice)
+  }
+
+  const disabledInfo = {...ingredients}
+
+  for (const key in disabledInfo) {
+    disabledInfo[key] = disabledInfo[key] <= 0
+  }
+
   return (
     <>
-      <div>Burger will be here</div>
-      <div>Build controls will be here</div>
+      <Burger ingredients={ingredients} />
+      <BuildControls 
+        ingredients={ingredients}
+        price={totalPrice}
+        ingredientAdded={addIngredientHandler}
+        ingredientRemoved={removeIngredientHandler}
+        disabledInfo={disabledInfo}
+      />
     </>
   )
 }
